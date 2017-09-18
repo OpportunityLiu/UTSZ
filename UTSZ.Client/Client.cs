31d1b75256e4f16f277873e36e555e0aac438cd9
Opportunity <liuzhengyuan13@outlook.com>
2017-09-18 16:22:12 +0800

    console ver

---
+++ UTSZ.Client/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UTSZ.Client
+{
+    public static class Client
+    {
+        private static HttpClient client = new HttpClient
+        {
+
+        };
+
+        public static async Task ConnentAsync(string username, string password)
+        {
+            var ac_test = await client.GetAsync("http://msconnecttest.com");
+            if (ac_test.RequestMessage.RequestUri.Host == "msconnecttest.com")
+                return;
+            var ac_id = string.Concat(ac_test.RequestMessage.RequestUri.PathAndQuery.Where(char.IsDigit));
+            if (string.IsNullOrEmpty(ac_id))
+                ac_id = "1";
+            var r = await client.PostAsync("http://10.0.10.66/include/auth_action.php", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                ["action"] = "login",
+                ["username"] = username,
+                ["password"] = password,
+                ["ac_id"] = ac_id,
+                ["ajax"] = "1"
+            }));
+            var data = await r.Content.ReadAsStringAsync();
+            if (data.StartsWith("login_ok"))
+                return;
+            throw new InvalidOperationException(data);
+        }
+
+        public static async Task DisconnentAsync()
+        {
+            var r = await client.PostAsync("http://10.0.10.66/include/auth_action.php", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                ["action"] = "logout",
+                ["ajax"] = "1"
+            }));
+            var data = await r.Content.ReadAsStringAsync();
+            if (data == "网络已断开")
+                return;
+            throw new InvalidOperationException(data);
+        }
+    }
+}
---
+++ UTSZ.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTSZ.Client;
+
+namespace UTSZ.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                try
+                {
+                    Client.Client.DisconnentAsync().Wait();
+                    System.Console.WriteLine("网络已断开");
+                }
+                catch (AggregateException ex)
+                {
+                    System.Console.Error.WriteLine(ex.InnerException.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    Client.Client.ConnentAsync(args[0], args[1]).Wait();
+                    System.Console.WriteLine("网络已连接");
+                }
+                catch (AggregateException ex)
+                {
+                    System.Console.Error.WriteLine(ex.InnerException.Message);
+                }
+            }
+        }
+    }
+}
